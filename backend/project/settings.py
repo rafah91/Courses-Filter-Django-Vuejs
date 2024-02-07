eec9c1aff17b719b59437e069ce66614c9058e87
rafah <rafah.hawa@yahoo.fr>
2024-02-07 22:24:20 +0100

    add django rest swagger

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
-    #'drf_yasg',
+    'drf_yasg',
     #"corsheaders",
     #'django_filters',
     
--- backend/project/urls.py
+++ backend/project/urls.py
@@ -18,12 +18,30 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Courses API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 from courses.views import CourseDetailAPI,CourseListAPI,CategoryListAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
     path('api/courses' , CourseListAPI.as_view()),
     path('api/category' , CategoryListAPI.as_view()),
