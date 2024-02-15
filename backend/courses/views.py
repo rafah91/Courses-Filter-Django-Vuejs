4d1754ffe1dd8074197d6adcc8385329e0f35bf6
rafah <rafah.hawa@yahoo.fr>
2024-02-15 16:47:14 +0100

    add viewsets

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework import filters
 
 from django_filters.rest_framework import DjangoFilterBackend
@@ -7,21 +8,26 @@ from .serializers import CourseSerializer , CategorySerializer
 from .models import Course , Category
 from .filters import CourseFilter
 
-
-
-class CourseListAPI(generics.ListAPIView):
+class CourseViewset(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['category']
+    filterset_fields = ['price','category']
     filterset_class = CourseFilter
+
+# class CourseListAPI(generics.ListAPIView):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializer
+#     filter_backends = [DjangoFilterBackend]
+#     filterset_fields = ['price','category']
+#     filterset_class = CourseFilter
     
 
     
     
-class CourseDetailAPI(generics.RetrieveAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
+# class CourseDetailAPI(generics.RetrieveAPIView):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializer
     
     
     
